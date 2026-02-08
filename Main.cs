7b72fb60e2a2eb69f9c204482cd0b1b129bbcd81
kirjorjos <kirjorjos@gmail.com>
2026-02-08 17:18:08 -0500

    Make basic title screen

---
+++ Main.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public partial class Main : Node2D
+{
+	public void onQuitPressed() {
+		GetTree().Quit();
+	}
+
+	public void onPlayPressed()
+	{
+		// start the game
+	}
+}
